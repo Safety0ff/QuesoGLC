eea3120898a4ff5327917c6362cb5e8c45e9d1b0
Bertrand Coconnier <bcoconni@gmail.com>
2002-09-12 00:02:15 +0000

    Custom memory management routines

---
+++ src/misc.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "internal.h"
+
+/* QuesoGLC own allocation and memory management routines */
+
+void* __glcMalloc(size_t size)
+{
+  return malloc(size);
+}
+
+void __glcFree(void *ptr)
+{
+  free(ptr);
+}
+
+void* __glcRealloc(void *ptr, size_t size)
+{
+  return realloc(ptr, size);
+}
