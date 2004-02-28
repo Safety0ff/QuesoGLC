4af6688796b9d4c12e92e147e27247c3be8d8662
Bertrand Coconnier <bcoconni@gmail.com>
2004-02-28 19:38:14 +0000

    Updated the String List API

--- tests/teststrlst.c
+++ tests/teststrlst.c
@@ -2,80 +2,124 @@
 #include "internal.h"
 #include <stdio.h>
 
+void display_strlst(FT_List This)
+{
+  FT_ListNode node = NULL;
+  __glcUniChar* ustring = NULL;
+
+  node = This->head;
+  while (node) {
+    ustring = (__glcUniChar *)node->data;
+    printf("%s<end>", (char *)ustring->ptr);
+    node = node->next;
+  }
+  printf("\nLength : %d\n", __glcStrLstLen(This));
+}
+
 void testQueso(void) {
-    __glcStringList sl;
-    __glcContextState state;
-    char *string[] = { "string 1", "string 2", "string 3", "string 4", "foobar" };
-    int i = 0;
-    char buffer[256];
-    
-    state.stringType = GLC_UCS1;
-    __glcStringListInit(&sl, &state);
+  FT_List sl;
+  __glcUniChar s;
+  __glcUniChar *ustring = NULL;
+  char *string[] = { "string 1", "string 2", "string 3", "string 4", "foobar" };
+  int i = 0;
+  GLCenum err;
 
-    printf("Append:\n");
-    for (i=0; i<3; i++) {
-	__glcStringListAppend(&sl, string[i]);
-	printf("%s<end> element : %d\n", (char *)sl.list, sl.count);
-    }
-    printf("Prepend:\n");
-    __glcStringListPrepend(&sl, string[3]);
-    printf("%s<end> element : %d\n", (char *)sl.list, sl.count);
+  /* Call in order to initialize the library */
+  err = glcGetError();
 
-    printf("FindIndex:\n");
-    for (i=0; i<5; i++)
-	printf("%d : %s<end>\n", i, (char *)__glcStringListFindIndex(&sl, i));
-    i = 10;
-    printf("%d : %s<end>\n", i, (char *)__glcStringListFindIndex(&sl, i));
+  sl = __glcStrLstCreate(NULL);
 
-    printf("Find:\n");
-    for (i=0; i<5; i++)
-	printf("%s : %s<end>\n", string[i], (char *)__glcStringListFind(&sl, string[i]));
+  printf("Append:\n");
+  for (i=0; i<3; i++) {
 
-    printf("Remove:\n");
-    __glcStringListRemove(&sl, string[0]);
-    printf("%s : %s<end> element : %d\n", string[0], (char *)sl.list, sl.count);
-    __glcStringListRemove(&sl, string[2]);
-    printf("%s : %s<end> element : %d\n", string[2], (char *)sl.list, sl.count);
-    __glcStringListRemove(&sl, string[3]);
-    printf("%s : %s<end> element : %d\n", string[3], (char *)sl.list, sl.count);
-    __glcStringListRemove(&sl, string[4]);
-    printf("%s : %s<end> element : %d\n", string[4], (char *)sl.list, sl.count);
+    s.ptr = string[i];
+    s.type = GLC_UCS1;
+    __glcStrLstAppend(sl, &s);
+    display_strlst(sl);
+  }
 
-    printf("\n");
-    for (i=0; i<2; i++) {
-	__glcStringListAppend(&sl, string[i]);
-    }
-    __glcStringListPrepend(&sl, string[3]);
-    printf("%s<end> element : %d\n", (char *)sl.list, sl.count);
+  printf("\nPrepend:\n");
+  s.ptr = string[3];
+  __glcStrLstPrepend(sl, &s);
+  display_strlst(sl);
 
-    printf("RemoveIndex:\n");
-    __glcStringListRemoveIndex(&sl, 1);
-    printf("%d : %s<end> element : %d\n", 1, (char *)sl.list, sl.count);
-    __glcStringListRemoveIndex(&sl, 0);
-    printf("%d : %s<end> element : %d\n", 0, (char *)sl.list, sl.count);
-    __glcStringListRemoveIndex(&sl, 1);
-    printf("%d : %s<end> element : %d\n", 1, (char *)sl.list, sl.count);
-    __glcStringListRemoveIndex(&sl, 10);
-    printf("%d : %s<end> element : %d\n", 10, (char *)sl.list, sl.count);
-    __glcStringListRemoveIndex(&sl, 0);
-    printf("%d : %s<end> element : %d\n", 0, (char *)sl.list, sl.count);
-    __glcStringListRemoveIndex(&sl, 10);
-    printf("%d : %s<end> element : %d\n", 10, (char *)sl.list, sl.count);
+  printf("\nFindIndex:\n");
+  for (i=0; i<5; i++) {
+    ustring = __glcStrLstFindIndex(sl, i);
+    if (ustring)
+      printf("%d : %s<end>\n", i, (char *)ustring->ptr);
+    else
+      printf("Index %d not found\n", i);
+  }
+  i = 10;
+  ustring = __glcStrLstFindIndex(sl, i);
+  if (ustring)
+    printf("%d : %s<end>\n", i, (char *)ustring->ptr);
+  else
+    printf("Index %d not found\n", i);
 
-    printf("\n");
-    for (i=0; i<2; i++) {
-	__glcStringListAppend(&sl, string[i]);
-    }
-    __glcStringListPrepend(&sl, string[3]);
-    printf("%s<end>\n", (char *)sl.list);
+  printf("\nRemove:\n");
+  s.ptr = string[0];
+  __glcStrLstRemove(sl, &s);
+  printf("Removed %s\n", string[0]);
+  display_strlst(sl);
 
-    printf("GetIndex:\n");
-    for(i = 0; i < 5; i++)
-	printf("%s index %d\n", string[i], __glcStringListGetIndex(&sl, string[i]));
-    
-    printf("Extract Element:\n");
-    for(i = 0; i < 5; i++) {
-	printf("String : %s elements : %d\n", (char *)sl.list, sl.count);
-	printf("element #%d : %s\n", i, (char *)__glcStringListExtractElement(&sl, i, buffer, 256));
-    }
+  for (i = 2; i < 5; i++) {
+    s.ptr = string[i];
+    __glcStrLstRemove(sl, &s);
+    printf("Removed %s\n", string[i]);
+    display_strlst(sl);
+  }
+
+  printf("\n");
+  for (i=0; i<2; i++) {
+    s.ptr = string[i];
+    __glcStrLstAppend(sl, &s);
+  }
+  s.ptr = string[3];
+  __glcStrLstPrepend(sl, &s);
+  display_strlst(sl);
+
+  printf("\nRemoveIndex:\n");
+  i = 1;
+  printf("Remove index %d\n", i);
+  __glcStrLstRemoveIndex(sl, i);
+  display_strlst(sl);
+  i = 0;
+  printf("Remove index %d\n", i);
+  __glcStrLstRemoveIndex(sl, i);
+  display_strlst(sl);
+  i = 1;
+  printf("Remove index %d\n", i);
+  __glcStrLstRemoveIndex(sl, i);
+  display_strlst(sl);
+  i = 10;
+  printf("Remove index %d\n", i);
+  __glcStrLstRemoveIndex(sl, i);
+  display_strlst(sl);
+  i = 0;
+  printf("Remove index %d\n", i);
+  __glcStrLstRemoveIndex(sl, i);
+  display_strlst(sl);
+  i = 10;
+  printf("Remove index %d\n", i);
+  __glcStrLstRemoveIndex(sl, i);
+  display_strlst(sl);
+
+  printf("\n");
+  for (i=0; i<2; i++) {
+    s.ptr = string[i];
+    __glcStrLstAppend(sl, &s);
+  }
+  s.ptr = string[3];
+  __glcStrLstPrepend(sl, &s);
+  display_strlst(sl);
+
+  printf("\nGetIndex:\n");
+  for(i = 0; i < 5; i++) {
+    s.ptr = string[i];
+    printf("%s index %d\n", string[i], __glcStrLstGetIndex(sl, &s));
+  }
+
+  __glcStrLstDestroy(sl);
 }
