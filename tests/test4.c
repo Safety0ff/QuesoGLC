4a93bdef11c85a2ca91e52ca4cb9f1afea875533
Bertrand Coconnier <bcoconni@gmail.com>
2004-11-07 17:14:56 +0000

    Fixed invalid preprocessing tokens

--- tests/test4.c
+++ tests/test4.c
@@ -8,12 +8,12 @@
 #define TestErrorCode(func) \
     err = glcGetError(); \
     if (err != GLC_STATE_ERROR) { \
-        printf(##func"() Unexpected error : 0x%x\n", (int)err); \
+        printf(#func"() Unexpected error : 0x%x\n", (int)err); \
         return -1; \
     } \
     err = glcGetError(); \
     if (err) { \
-        printf("Unexpected error : 0x%x\n", (int)err); \
+        printf(#func"Error is not GLC_NONE but : 0x%x\n", (int)err); \
         return -1; \
     }
 
@@ -30,173 +30,173 @@ int main(void)
     /* Context commands */
 
     glcCallbackFunc(GLC_OP_glcUnmappedCode, NULL);
-    TestErrorCode("glcCallbackFunc");
+    TestErrorCode(glcCallbackFunc);
 
     glcDataPointer(NULL);
-    TestErrorCode("glcDataPointer");
+    TestErrorCode(glcDataPointer);
 
     glcDeleteGLObjects();
-    TestErrorCode("glcDeleteGLObjects");
+    TestErrorCode(glcDeleteGLObjects);
 
     glcDisable(GLC_MIPMAP);
-    TestErrorCode("glcDisable");
+    TestErrorCode(glcDisable);
 
     glcEnable(GLC_GL_OBJECTS);
-    TestErrorCode("glcEnable");
+    TestErrorCode(glcEnable);
 
     glcGetCallbackFunc(GLC_OP_glcUnmappedCode);
-    TestErrorCode("glcGetCallbackFunc");
+    TestErrorCode(glcGetCallbackFunc);
 
     glcGetListc(GLC_CATALOG_LIST, 0);
-    TestErrorCode("glcGetListc");
+    TestErrorCode(glcGetListc);
 
     glcGetListi(GLC_FONT_LIST, 0);
-    TestErrorCode("glcGetListi");
+    TestErrorCode(glcGetListi);
 
     glcGetPointer(GLC_DATA_POINTER);
-    TestErrorCode("glcGetPointer");
+    TestErrorCode(glcGetPointer);
 
     glcGetc(GLC_RELEASE);
-    TestErrorCode("glcGetc");
+    TestErrorCode(glcGetc);
 
     glcGetf(GLC_RESOLUTION);
-    TestErrorCode("glcGetf");
+    TestErrorCode(glcGetf);
 
     glcGetfv(GLC_BITMAP_MATRIX, NULL);
-    TestErrorCode("glcGetfv");
+    TestErrorCode(glcGetfv);
 
     glcGeti(GLC_VERSION_MAJOR);
-    TestErrorCode("glcGeti");
+    TestErrorCode(glcGeti);
 
     glcIsEnabled(GLC_AUTO_FONT);
-    TestErrorCode("glcIsEnabled");
+    TestErrorCode(glcIsEnabled);
 
     glcStringType(GLC_UCS1);
-    TestErrorCode("glcStringType");
+    TestErrorCode(glcStringType);
 
     /* Master commands */
 
     glcAppendCatalog(NULL);
-    TestErrorCode("glcAppendCatalog");
+    TestErrorCode(glcAppendCatalog);
 
     glcGetMasterListc(0, GLC_CHAR_LIST, 0);
-    TestErrorCode("glcGetMasterListc");
+    TestErrorCode(glcGetMasterListc);
 
     glcGetMasterMap(0,0);
-    TestErrorCode("glcGetMasterMap");
+    TestErrorCode(glcGetMasterMap);
 
     glcGetMasterc(0, GLC_FAMILY);
-    TestErrorCode("glcGetMasterc");
+    TestErrorCode(glcGetMasterc);
 
     glcGetMasteri(0, GLC_CHAR_COUNT);
-    TestErrorCode("glcGetMasteri");
+    TestErrorCode(glcGetMasteri);
 
     glcPrependCatalog(NULL);
-    TestErrorCode("glcPrependCatalog");
+    TestErrorCode(glcPrependCatalog);
 
     glcRemoveCatalog(0);
-    TestErrorCode("glcRemoveCatalog");
+    TestErrorCode(glcRemoveCatalog);
 
     /* Font commands */
 
     glcAppendFont(0);
-    TestErrorCode("glcAppendFont");
+    TestErrorCode(glcAppendFont);
 
     glcDeleteFont(0);
-    TestErrorCode("glcDeleteFont");
+    TestErrorCode(glcDeleteFont);
 
     glcFont(0);
-    TestErrorCode("glcFont");
+    TestErrorCode(glcFont);
 
     glcFontFace(0, NULL);
-    TestErrorCode("glcFontFace");
+    TestErrorCode(glcFontFace);
 
     glcFontMap(0, 0, NULL);
-    TestErrorCode("glcFontMap");
+    TestErrorCode(glcFontMap);
 
     glcGenFontID();
-    TestErrorCode("glcGenFontID");
+    TestErrorCode(glcGenFontID);
 
     glcGetFontFace(0);
-    TestErrorCode("glcGetFontFace");
+    TestErrorCode(glcGetFontFace);
 
     glcGetFontListc(0, GLC_CHAR_LIST, 0);
-    TestErrorCode("glcGetFontListc");
+    TestErrorCode(glcGetFontListc);
 
     glcGetFontMap(0, 0);
-    TestErrorCode("glcGetFontMap");
+    TestErrorCode(glcGetFontMap);
 
     glcGetFontc(0, GLC_FAMILY);
-    TestErrorCode("glcGetFontc");
+    TestErrorCode(glcGetFontc);
 
     glcGetFonti(0, GLC_IS_FIXED_PITCH);
-    TestErrorCode("glcGetFonti");
+    TestErrorCode(glcGetFonti);
 
     glcIsFont(0);
-    TestErrorCode("glcIsFont");
+    TestErrorCode(glcIsFont);
 
     glcNewFontFromFamily(1, NULL);
-    TestErrorCode("glcNewFontFromFamily");
+    TestErrorCode(glcNewFontFromFamily);
 
     glcNewFontFromMaster(1, 0);
-    TestErrorCode("glcNewFontFromMaster");
+    TestErrorCode(glcNewFontFromMaster);
 
     /* Transformation commands */
 
     glcLoadIdentity();
-    TestErrorCode("glcLoadIdentity");
+    TestErrorCode(glcLoadIdentity);
 
     glcLoadMatrix(NULL);
-    TestErrorCode("glcLoadMatrix");
+    TestErrorCode(glcLoadMatrix);
 
     glcMultMatrix(NULL);
-    TestErrorCode("glcMultMatrix");
+    TestErrorCode(glcMultMatrix);
 
     glcRotate(0.);
-    TestErrorCode("glcRotate");
+    TestErrorCode(glcRotate);
 
     glcScale(1., 1.);
-    TestErrorCode("glcScale");
+    TestErrorCode(glcScale);
 
     /* Rendering commands */
 
     glcRenderChar(0);
-    TestErrorCode("glcRenderChar");
+    TestErrorCode(glcRenderChar);
 
     glcRenderCountedString(0, NULL);
-    TestErrorCode("glcRenderCountedString");
+    TestErrorCode(glcRenderCountedString);
 
     glcRenderString(NULL);
-    TestErrorCode("glcRenderString");
+    TestErrorCode(glcRenderString);
 
     glcRenderStyle(GLC_LINE);
-    TestErrorCode("glcRenderStyle");
+    TestErrorCode(glcRenderStyle);
 
     glcReplacementCode(0);
-    TestErrorCode("glcReplacementCode");
+    TestErrorCode(glcReplacementCode);
 
     glcResolution(0.);
-    TestErrorCode("glcResolution");
+    TestErrorCode(glcResolution);
 
     /* Measurement commands*/
 
     glcGetCharMetric(0, GLC_BOUNDS, NULL);
-    TestErrorCode("glcGetCharMetric");
+    TestErrorCode(glcGetCharMetric);
 
     glcGetMaxCharMetric(GLC_BASELINE, NULL);
-    TestErrorCode("glcGetMaxCharMetric");
+    TestErrorCode(glcGetMaxCharMetric);
 
     glcGetStringCharMetric(0, GLC_BOUNDS, NULL);
-    TestErrorCode("glcGetStringCharMetric");
+    TestErrorCode(glcGetStringCharMetric);
 
     glcGetStringMetric(GLC_BOUNDS, 0);
-    TestErrorCode("glcGetStringMetric");
+    TestErrorCode(glcGetStringMetric);
 
     glcMeasureCountedString(GL_TRUE, 1, NULL);
-    TestErrorCode("glcMeasureCountedString");
+    TestErrorCode(glcMeasureCountedString);
 
     glcMeasureString(GL_FALSE, NULL);
-    TestErrorCode("glcMeasureString");
+    TestErrorCode(glcMeasureString);
 
     printf("Tests successful !\n");
     return 0;
