1cd10933bb3e1ca0997589b13177ee6b5e530139
Bertrand Coconnier <bcoconni@gmail.com>
2005-09-24 11:45:07 +0000

    - General management of header files has been moved to internal.h - Removed the C++ detection code since this header is internal to QuesoGLC (hence its name) which is a pure C library

--- include/internal.h
+++ include/internal.h
@@ -35,110 +35,109 @@
 #include "ofont.h"
 #include "ocontext.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #define GLC_OUT_OF_RANGE_LEN 11
 
-  typedef struct {
-    FT_ListNodeRec node;
-    __glcFaceDescriptor* faceDesc;
-    GLint code;
-    GLint renderMode;
-    GLuint list;
-  } __glcDisplayListKey;
+typedef struct {
+  FT_ListNodeRec node;
+  __glcFaceDescriptor* faceDesc;
+  GLint code;
+  GLint renderMode;
+  GLuint list;
+} __glcDisplayListKey;
 
-  typedef struct {
-    GLint code;
-    GLCchar* name;
-  } __glcDataCodeFromName;
+typedef struct {
+  GLint code;
+  GLCchar* name;
+} __glcDataCodeFromName;
 
-  /* Character renderers */
-  extern void __glcRenderCharScalable(__glcFont* inFont,
-				      __glcContextState* inState, GLint inCode,
-				      GLboolean inFill);
+/* Character renderers */
+extern void __glcRenderCharScalable(__glcFont* inFont,
+				    __glcContextState* inState, GLint inCode,
+				    GLboolean inFill, FT_Face inFace);
 
-  /* QuesoGLC own memory management routines */
-  extern void* __glcMalloc(size_t size);
-  extern void __glcFree(void* ptr);
-  extern void* __glcRealloc(void* ptr, size_t size);
+/* QuesoGLC own memory management routines */
+extern void* __glcMalloc(size_t size);
+extern void __glcFree(void* ptr);
+extern void* __glcRealloc(void* ptr, size_t size);
 
-  /* Find a token in a list of tokens separated by 'separator' */
-  extern GLCchar* __glcFindIndexList(const GLCchar* inList, GLuint inIndex,
-				     const GLCchar* inSeparator);
+/* Find a token in a list of tokens separated by 'separator' */
+extern GLCchar* __glcFindIndexList(const GLCchar* inList, GLuint inIndex,
+				   const GLCchar* inSeparator);
 
-  /* Deletes GL objects defined in context state */
-  extern void __glcDeleteGLObjects(__glcContextState *inState);
+/* Deletes GL objects defined in context state */
+extern void __glcDeleteGLObjects(__glcContextState *inState);
 
-  extern const __glcDataCodeFromName __glcCodeFromNameArray[];
-  extern const GLint __glcNameFromCodeArray[];
-  extern const GLint __glcMaxCode;
-  extern const GLint __glcCodeFromNameSize;
-  /* Find a Unicode name from its code */
-  extern GLCchar* __glcNameFromCode(GLint code);
-  /* Find a Unicode code from its name */
-  extern GLint __glcCodeFromName(GLCchar* name);
+extern const __glcDataCodeFromName __glcCodeFromNameArray[];
+extern const GLint __glcNameFromCodeArray[];
+extern const GLint __glcMaxCode;
+extern const GLint __glcCodeFromNameSize;
 
-  /* Duplicate a string in UTF-8 format */
-  extern FcChar8* __glcConvertToUtf8(const GLCchar* inString,
+/* Find a Unicode name from its code */
+extern GLCchar* __glcNameFromCode(GLint code);
+
+/* Find a Unicode code from its name */
+extern GLint __glcCodeFromName(GLCchar* name);
+
+/* Duplicate a string in UTF-8 format */
+extern FcChar8* __glcConvertToUtf8(const GLCchar* inString,
+				   const GLint inStringType);
+
+/* Duplicate a UTF-8 string to another format */
+extern GLCchar* __glcConvertFromUtf8(const FcChar8* inString,
 				     const GLint inStringType);
-  /* Duplicate a UTF-8 string to another format */
-  extern GLCchar* __glcConvertFromUtf8(const FcChar8* inString,
-				       const GLint inStringType);
 
-  /* Duplicate a UTF-8 string to the context buffer */
-  extern GLCchar* __glcConvertFromUtf8ToBuffer(__glcContextState* This,
-					       const FcChar8* inString,
-					       const GLint inStringType);
+/* Duplicate a UTF-8 string to the context buffer */
+extern GLCchar* __glcConvertFromUtf8ToBuffer(__glcContextState* This,
+					     const FcChar8* inString,
+					     const GLint inStringType);
 
-  /* Duplicate a counted string in UTF-8 format */
-  FcChar8* __glcConvertCountedStringToUtf8(const GLint inCount,
-					   const GLCchar* inString,
-					   const GLint inStringType);
+/* Duplicate a counted string in UTF-8 format */
+FcChar8* __glcConvertCountedStringToUtf8(const GLint inCount,
+					 const GLCchar* inString,
+					 const GLint inStringType);
 
-  /* Count the number of bits that are set in c1  */
-  extern FcChar32 __glcCharSetPopCount(FcChar32 c1);
+/* Count the number of bits that are set in c1  */
+extern FcChar32 __glcCharSetPopCount(FcChar32 c1);
 
-  /* This function is called to destroy texture objects */
-  extern void __glcTextureObjectDestructor(FT_Memory inMemory, void *inData,
-					   void *inUser);
+/* This function is called to destroy texture objects */
+extern void __glcTextureObjectDestructor(FT_Memory inMemory, void *inData,
+					 void *inUser);
 
-  /* This function is called to destroy display lists */
-  extern void __glcDisplayListDestructor(FT_Memory inMemory, void *inData,
-					   void *inUser);
+/* This function is called to destroy display lists */
+extern void __glcDisplayListDestructor(FT_Memory inMemory, void *inData,
+				       void *inUser);
 
-  /* This function parses the font set and add the font files to the masters
-   * of the context 'This'. Masters are created if necessary.
-   */
-  void __glcAddFontsToContext(__glcContextState *This, FcFontSet *fontSet,
-			      GLboolean inAppend);
+/* Convert a UCS-4 character code into the current string type. The result is
+ * stored in a GLint. This function is needed since the GLC specs store
+ * individual character codes in GLint which may cause problems for the UTF-8
+ * format.
+ */
+GLint __glcConvertUcs4ToGLint(__glcContextState *inState, GLint inCode);
 
-  /* Convert a UCS-4 character code into the current string type. The result is
-   * stored in a GLint. This function is needed since the GLC specs store
-   * individual character codes in GLint which may cause problems for the UTF-8
-   * format.
-   */
-  GLint __glcConvertUcs4ToGLint(__glcContextState *inState, GLint inCode);
+/* Convert a character encoded in the current string type to the UCS-4 format.
+ * This function is needed since the GLC specs store individual character
+ * codes in GLint which may cause problems for the UTF-8 format.
+ */
+GLint __glcConvertGLintToUcs4(__glcContextState *inState, GLint inCode);
 
-  /* Convert a character encoded in the current string type to the UCS-4 format.
-   * This function is needed since the GLC specs store individual character
-   * codes in GLint which may cause problems for the UTF-8 format.
-   */
-  GLint __glcConvertGLintToUcs4(__glcContextState *inState, GLint inCode);
+/* Verify that the thread has a current context and that the master identified
+ * by 'inMaster' exists.
+ */
+__glcMaster* __glcVerifyMasterParameters(GLint inMaster);
 
-  /* Verify that the thread has a current context and that the master identified
-   * by 'inMaster' exists.
-   */
-  __glcMaster* __glcVerifyMasterParameters(GLint inMaster);
+/* Get the minimum mapped code of a character set */
+GLint __glcGetMinMappedCode(FcCharSet *charSet);
 
-  /* Get the minimum mapped code of a character set */
-  GLint __glcGetMinMappedCode(FcCharSet *charSet);
+/* Get the maximum mapped code of a character set */
+GLint __glcGetMaxMappedCode(FcCharSet *charSet);
 
-  /* Get the maximum mapped code of a character set */
-  GLint __glcGetMaxMappedCode(FcCharSet *charSet);
-#ifdef __cplusplus
-}
-#endif
+/* Return a struct which contains thread specific info */
+threadArea* __glcGetThreadArea(void);
+
+/* Raise an error */
+void __glcRaiseError(GLCenum inError);
+
+/* Return the current context state */
+__glcContextState* __glcGetCurrent(void);
 
 #endif /* __glc_internal_h */
