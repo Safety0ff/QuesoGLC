14758d10e2f16a1d4a5b30c9ac5deb44227de857
Bertrand Coconnier <bcoconni@gmail.com>
2006-08-26 12:25:32 +0000

    Added LGPL license notice and a command-line parameter to specify the number of Unicode character names that will be hard-coded in the library

--- database/buildDB.py
+++ database/buildDB.py
@@ -1,5 +1,31 @@
 #!/usr/bin/python
-import urllib, string
+
+# QuesoGLC
+# A free implementation of the OpenGL Character Renderer (GLC)
+# Copyright (c) 2002, 2004-2006, Bertrand Coconnier
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# $Id$
+
+import sys, urllib, string
+
+if len(sys.argv) > 1:
+    numchars = int(sys.argv[1])
+else:
+    numchars = 256
 
 print "Open URL..."
 unicodata = urllib.urlopen("http://www.unicode.org/Public/UNIDATA/UnicodeData.txt")
@@ -23,8 +49,8 @@ for s in lignes:
 liste = []
 codes = unicodeNameFromCode.keys()
 codes.sort()
-vmax = max(codes[:256])
-for c in codes[:256]:
+vmax = max(codes[:numchars])
+for c in codes[:numchars]:
     liste.append(unicodeNameFromCode[c])
 liste.sort()
 i = 0
