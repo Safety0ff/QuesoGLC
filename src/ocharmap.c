f3b4d684d882e14ce0e70af013497c92485d84f8
Bertrand Coconnier <bcoconni@gmail.com>
2007-01-21 20:01:19 +0000

    QuesoGLC now uses GLEW to manage OpenGL extensions

--- src/ocharmap.c
+++ src/ocharmap.c
@@ -1,6 +1,6 @@
 /* QuesoGLC
  * A free implementation of the OpenGL Character Renderer (GLC)
- * Copyright (c) 2002, 2004-2006, Bertrand Coconnier
+ * Copyright (c) 2002, 2004-2007, Bertrand Coconnier
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
@@ -20,7 +20,6 @@
 
 #include <fontconfig/fontconfig.h>
 
-#include "ocharmap.h"
 #include "internal.h"
 
 /** \file
